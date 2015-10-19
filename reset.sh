4fa217fb8a62188ce21aca8d7b5ad37d7211e29b
Kevin Benton <blak111@gmail.com>
2015-10-19 03:21:40 -0700

    Cleanups
    * fix path issue
    * Fix venv issue with neutron server not getting setup right

--- reset.sh
+++ reset.sh
@@ -2,6 +2,8 @@
 cd /opt/stack/neutron/
 echo "drop database neutron;" | mysql -u root -pkevin
 echo "create database neutron;" | mysql -u root -pkevin
+tox -vr -epy27 noopas
 source .tox/py27/bin/activate
 pip install .
+pip install pymysql
 .tox/py27/bin/neutron-db-manage --config-file /etc/neutron/neutron.conf upgrade head
