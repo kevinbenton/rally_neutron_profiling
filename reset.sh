382420b2666b5c9c74f7f8b2120c0034a36618c8
Kevin Benton <blak111@gmail.com>
2015-10-19 03:51:03 -0700

    more cleanup commands

--- reset.sh
+++ reset.sh
@@ -2,6 +2,10 @@
 cd /opt/stack/neutron/
 echo "drop database neutron;" | mysql -u root -pkevin
 echo "create database neutron;" | mysql -u root -pkevin
+sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron
+sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron.egg-link
+sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron-*
+sudo rm -rf .tox/py27
 tox -vr -epy27 noopas
 source .tox/py27/bin/activate
 pip install .
