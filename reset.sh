41b741a9cd35ff5f8b4b86fbdbbc8e055f4fbdb8
Kevin Benton <blak111@gmail.com>
2015-10-21 02:11:27 -0700

    handle tox incompatibilities

--- reset.sh
+++ reset.sh
@@ -6,7 +6,16 @@ sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron
 sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron.egg-link
 sudo rm -rf /usr/local/lib/python2.7/dist-packages/neutron-*
 sudo rm -rf .tox/py27
-tox -vr -epy27 noopas
+find . -name "*.pyc" -exec rm -rf {} \;
+tox -vr -epy27 test_exceptions_notimplemented
+if [ $? -ne 0 ]; then
+    sudo pip install tox==1.9
+    tox -vr -epy27 test_exceptions_notimplemented
+fi
+if [ $? -ne 0 ]; then
+    sudo pip install tox==2.1.1
+    tox -vr -epy27 test_exceptions_notimplemented
+fi
 source .tox/py27/bin/activate
 pip install .
 pip install pymysql
